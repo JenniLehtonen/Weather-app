ef307b36bb79ee746d3c2ea0c10e7634246f344b
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2021-06-25 12:03:31 +0300

    Create a few components and a basic UI

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css'
+import {Container} from 'react-bootstrap';
+import Search from './components/Search'
+import CurrentWeather from './components/CurrentWeather'
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+       Weather app
+      </Container>
+      <Container fluid className="weather">
+        <Search />
+        <CurrentWeather />
+      </Container>
     </div>
   );
 }
---
+++ weather-app/src/components/CurrentWeather.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CurrentWeather = () => {
+    return(
+        <div className="currentWeather">
+            <h1>Current weather in _____________</h1>
+            
+        </div>
+    )
+}
+
+export default CurrentWeather;
