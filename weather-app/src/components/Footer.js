b006175c10d1b83561a10972e41fc1414f2d7b17
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2021-08-04 18:27:28 +0300

    Small UI changes

--- weather-app/src/components/Footer.js
+++ weather-app/src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => (
   <footer>
     <h5>©2021 Jenni Lehtonen</h5> <br/>
     <div>
-      <h6>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>, <a>Backgroundphoto by <a href="https://unsplash.com/@garyellisphoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Gary Ellis</a> on <a href="https://unsplash.com/s/photos/scotland?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></a>, <a>London photo by Joanna Zduńczyk from Pexels</a>, <a>Paris photo by Eugene Dorosh from Pexels</a>
-      </h6>
+      <h6>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> <br />
+      See the sources of the images of this app from my <a href="https://github.com/JenniLehtonen/Weather-app">Github repository</a></h6>
    </div>
   </footer>
 );
